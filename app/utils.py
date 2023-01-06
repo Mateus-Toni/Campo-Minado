682bf5a5a8fd456f57ace13c121616b6a8eb61b5
Mateus <95312522+Mateus-Toni@users.noreply.github.com>
2023-01-06 17:24:07 -0300

    new functions

--- app/utils.py
+++ app/utils.py
@@ -22,52 +22,151 @@ def show_table(table):
 
     print("-\t"*len(table))
 
+
+def change_caracter(caracter):
+
+    if caracter == 'X':
+
+        return '1'
+
+    elif caracter.isnumeric():
+
+        return str(int(caracter) + 1)
+
+    else: 
+
+        return caracter
+
+
 def populate_bomb(table):
 
     size_table = len(table) - 1
 
     for bomb in range(len(table)):
 
-        colunm = randint(0, size_table)
-        line = randint(0, size_table)
+        while True:
 
-        table[line][colunm] = 'B'
+            colunm = randint(0, size_table)
+            line = randint(0, size_table)
 
-        if 0 in [colunm, line] and size_table in [colunm, line]:
+            if table[line][colunm] != 'B':
 
-            pass
+                table[line][colunm] = 'B'
+                break
+
+        if line == 0 and colunm == 0: 
+
+            table[line + 1][colunm + 1] = change_caracter(table[line + 1][colunm + 1])
+            table[line][colunm + 1] = change_caracter(table[line][colunm + 1])
+            table[line + 1][colunm] = change_caracter(table[line + 1][colunm])
+
+        elif line == size_table and colunm == 0:
+
+            table[line - 1][colunm + 1] = change_caracter(table[line - 1][colunm + 1])
+            table[line - 1][colunm] = change_caracter(table[line - 1][colunm])
+            table[line][colunm + 1] = change_caracter(table[line][colunm + 1])
+
+        elif line == 0 and colunm == size_table:
+
+            table[line + 1][colunm - 1] = change_caracter(table[line + 1][colunm - 1])
+            table[line + 1][colunm] = change_caracter(table[line + 1][colunm])
+            table[line][colunm - 1] = change_caracter(table[line][colunm - 1])
+
+        elif line == size_table and colunm == size_table:
+
+            table[line - 1][colunm - 1] = change_caracter(table[line - 1][colunm - 1])
+            table[line][colunm - 1] = change_caracter(table[line][colunm - 1])
+            table[line - 1][colunm] = change_caracter(table[line - 1][colunm])
+
+        elif 0 == colunm and 0 < line < size_table:
+
+            table[line + 1][colunm + 1] = change_caracter(table[line + 1][colunm + 1])
+            table[line - 1][colunm + 1] = change_caracter(table[line - 1][colunm + 1])
+            table[line][colunm + 1] = change_caracter(table[line][colunm + 1])
+            table[line + 1][colunm] = change_caracter(table[line + 1][colunm])
+            table[line - 1][colunm] = change_caracter(table[line - 1][colunm])
+
+        elif size_table == colunm and 0 < line < size_table:
+
+            table[line - 1][colunm - 1] = change_caracter(table[line - 1][colunm - 1])
+            table[line + 1][colunm - 1] = change_caracter(table[line + 1][colunm - 1])
+            table[line][colunm - 1] = change_caracter(table[line][colunm - 1])
+            table[line + 1][colunm] = change_caracter(table[line + 1][colunm])
+            table[line - 1][colunm] = change_caracter(table[line - 1][colunm])
+
+        elif 0  == line and 0 < colunm < size_table:
+
+            table[line + 1][colunm + 1] = change_caracter(table[line + 1][colunm + 1])
+            table[line + 1][colunm - 1] = change_caracter(table[line + 1][colunm - 1])
+            table[line][colunm + 1] = change_caracter(table[line][colunm + 1])
+            table[line][colunm - 1] = change_caracter(table[line][colunm - 1])
+            table[line + 1][colunm] = change_caracter(table[line + 1][colunm])
+
+        elif size_table == line and 0 < colunm < size_table:
+
+            table[line - 1][colunm - 1] = change_caracter(table[line - 1][colunm - 1])
+            table[line - 1][colunm + 1] = change_caracter(table[line - 1][colunm + 1])
+            table[line][colunm + 1] = change_caracter(table[line][colunm + 1])
+            table[line][colunm - 1] = change_caracter(table[line][colunm - 1])
+            table[line - 1][colunm] = change_caracter(table[line - 1][colunm])
 
         else:
 
-            match line:
-
-                case 0:
-
-                    table[line + 1][colunm + 1] = 1 if table[line + 1][colunm + 1] == 'X' else table[line + 1][colunm + 1] + 1
-                    table[line + 1][colunm - 1] = 1 if table[line + 1][colunm - 1] == 'X' else table[line + 1][colunm - 1] + 1
-                    table[line + 1][colunm] = 1 if table[line + 1][colunm] == 'X' else table[line + 1][colunm] + 1
-                    table[line][colunm + 1] = 1 if table[line][colunm + 1] == 'X' else table[line][colunm + 1] + 1
-                    table[line][colunm - 1] = 1 if table[line][colunm - 1] == 'X' else table[line][colunm - 1] + 1
-
-
-                case size_table:
-
-                    table[line - 1][colunm + 1] = 1 if table[line + 1][colunm + 1] == 'X' else table[line + 1][colunm + 1] + 1
-                    table[line - 1][colunm - 1] = 1 if table[line + 1][colunm - 1] == 'X' else table[line + 1][colunm - 1] + 1
-                    table[line - 1][colunm] = 1 if table[line + 1][colunm] == 'X' else table[line + 1][colunm] + 1
-                    table[line][colunm + 1] = 1 if table[line][colunm + 1] == 'X' else table[line][colunm + 1] + 1
-                    table[line][colunm - 1] = 1 if table[line][colunm - 1] == 'X' else table[line][colunm - 1] + 1
-
-            match colunm:
-
-                case 0:
-
-                    table[line + 1][colunm + 1] = 1 if table[line][colunm] == 'X' else table[line][colunm] + 1
-                    table[line - 1][colunm + 1] = 1 if table[line][colunm] == 'X' else table[line][colunm] + 1
-                    table[line][colunm + 1] = 1 if table[line][colunm] == 'X' else table[line][colunm] + 1
-                    table[line + 1][colunm] = 1 if table[line][colunm] == 'X' else table[line][colunm] + 1
-                    table[line - 1][colunm] = 1 if table[line][colunm] == 'X' else table[line][colunm] + 1
+            table[line + 1][colunm + 1] = change_caracter(table[line + 1][colunm + 1])
+            table[line - 1][colunm - 1] = change_caracter(table[line - 1][colunm - 1])
+            table[line - 1][colunm + 1] = change_caracter(table[line - 1][colunm + 1])
+            table[line + 1][colunm - 1] = change_caracter(table[line + 1][colunm - 1])
+            table[line][colunm + 1] = change_caracter(table[line][colunm + 1])
+            table[line][colunm - 1] = change_caracter(table[line][colunm - 1])
+            table[line + 1][colunm] = change_caracter(table[line + 1][colunm])
+            table[line - 1][colunm] = change_caracter(table[line - 1][colunm])
 
     return table
 
-show_table(populate_bomb(create_table(10, 'X')))
+
+def get_free_area(table, line_p, colunm_p):
+
+    size_table = len(table) - 1
+
+    table[line_p][colunm_p] = 'F'
+
+    for count in range(len(table)):
+
+        if colunm_p == 0: 
+
+            for item in range(len(table[line_p])):
+
+                if table[line_p][item] in 'XF':
+
+                  table[line_p][item] = 'F'
+
+                else:
+
+                    break  
+
+        elif colunm_p == size_table:
+            
+            for item in range(len(table[line_p]), 0):
+
+                if table[line_p][item] in 'XF':
+
+                  table[line_p][item] = 'F'
+
+                else:
+
+                    break 
+            
+        elif 0 < colunm_p < size_table:
+
+            ...
+
+        
+
+
+    
+    
+table = create_table(size=10, caracter='X')
+
+table = populate_bomb(table)
+
+get_free_area(table, 5, 5)
