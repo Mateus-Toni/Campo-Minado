da5de939e20cdb9dc9207cc8f4bb317459e7b279
Mateus <95312522+Mateus-Toni@users.noreply.github.com>
2023-01-09 22:07:46 -0300

    finish game

---
+++ app/main.py
@@ -0,0 +1,79 @@
+import utils
+
+
+while True:
+    
+    print("""
+Choose your game size:
+1) Eazy - 5 Bombs
+2) Medium - 10 Bombs
+3) Hard - 20 Bombs
+    """)
+    
+    choose = int(input('your choose: '))
+
+    match choose:
+        case 1:
+
+            size = 5
+            break
+
+        case 2:
+
+            size = 10
+            break
+
+        case 3:
+
+            size = 20
+            break
+
+        case default:
+
+            print('wrong input')
+
+
+
+template = utils.create_table(size=size, caracter='X')
+template = utils.populate_bomb(table=template)
+game = utils.create_table(size=size, caracter='X')
+
+while True:
+
+    utils.show_table(game)
+
+    print("""
+your attempt:
+    """)
+
+    line = int(input('line: '))
+    colunm = int(input('colunm: '))
+
+    if template[line][colunm] == 'X':
+
+        index_free_area = [(line, colunm)]
+
+        index_free_area = utils.get_free_area(template, index_free_area)
+
+        for value in index_free_area:
+
+            line = value[0]
+            colunm = value[1]
+
+            template[line][colunm] = 'F'
+            game[line][colunm] = 'F'
+
+    elif template[line][colunm].isnumeric():
+
+        game[line][colunm] = template[line][colunm]
+
+    elif template[line][colunm] == 'B':
+
+        print('lose')
+
+    else:
+
+        print('this is a free area')
+
+
+
